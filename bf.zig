dd9141b7ac5ecead4d37d31649aee61b74d7415a
Jimmi Holst Christensen <jimmiholstchristensen@gmail.com>
2018-06-17 17:30:33 +0200

    Updated bf to return errors

--- bf.zig
+++ bf.zig
@@ -21,7 +21,7 @@ pub const MemInStream = struct {
         };
     }
 
-    fn readFn(in_stream: &Stream, buffer: []u8) Error!usize {
+    fn readFn(in_stream: *Stream, buffer: []u8) Error!usize {
         const self = @fieldParentPtr(MemInStream, "stream", in_stream);
         const bytes_read = math.min(buffer.len, self.memory.len);
 
@@ -48,7 +48,7 @@ pub const MemOutStream = struct {
         };
     }
 
-    fn writeFn(out_stream: &Stream, buffer: []const u8) Error!void {
+    fn writeFn(out_stream: *Stream, buffer: []const u8) Error!void {
         const self = @fieldParentPtr(MemOutStream, "stream", out_stream);
         const bytes_written = math.min(buffer.len, self.memory.len);
 
@@ -65,35 +65,73 @@ test "bf.interpret: Hello World" {
     var in_fs = MemInStream.init(in[0..]);
     var out_fs = MemOutStream.init(out[0..]);
     const program = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
-    interpret(program, tape[0..], &in_fs.stream, &out_fs.stream);
+    try interpret(program, tape[0..], &in_fs.stream, &out_fs.stream);
     debug.assert(mem.startsWith(u8, out, "Hello World!\n"));
 }
 
-pub fn interpret(program: []const u8, tape: []u8, in_stream: var, out_stream: var) void {
+fn load(m: []const u8, ptr: usize) !u8 {
+    if (m.len <= ptr)
+        return error.OutOfBounds;
+
+    return m[ptr];
+}
+
+fn store(m: []u8, ptr: usize, v: u8) !void {
+    if (m.len <= ptr)
+        return error.OutOfBounds;
+
+    m[ptr] = v;
+}
+
+fn add(m: []u8, ptr: usize, v: u8) !void {
+    var res: u8 = try load(m, ptr);
+    store(m, ptr, res +% v) catch unreachable;
+}
+
+fn sub(m: []u8, ptr: usize, v: u8) !void {
+    var res: u8 = try load(m, ptr);
+    store(m, ptr, res -% v) catch unreachable;
+}
+
+pub fn interpret(program: []const u8, tape: []u8, in_stream: var, out_stream: var) !void {
     var ip : usize = 0;
     var mp : usize = 0;
 
     while (ip < program.len) : (ip += 1) {
-        switch (program[ip]) {
-            '>' => mp += 1,
-            '<' => mp -= 1,
-            '+' => tape[mp] += 1,
-            '-' => tape[mp] -= 1,
-            '.' => out_stream.writeByte(tape[mp]) catch unreachable,
-            ',' => tape[mp] = in_stream.readByte() catch unreachable,
+        switch (try load(program, ip)) {
+            '>' => mp = math.add(usize, mp, 1) catch return error.OutOfBounds,
+            '<' => mp = math.sub(usize, mp, 1) catch return error.OutOfBounds,
+            '+' => try add(tape, mp, 1),
+            '-' => try sub(tape, mp, 1),
+            '.' => try out_stream.writeByte(try load(tape, mp)),
+            ',' => try store(tape, mp, try in_stream.readByte()),
             '[' => {
                 if (tape[mp] == 0) {
-                    while (program[ip] != ']')
-                        ip += 1;
+                    var skips: usize = 1;
+                    while (skips != 0) {
+                        ip = math.add(usize, ip, 1) catch return error.OutOfBounds;
+                        switch (try load(program, ip)) {
+                            '[' => skips += 1,
+                            ']' => skips -= 1,
+                            else => {}
+                        }
+                    }
                 }
             },
             ']' => {
                 if (tape[mp] != 0) {
-                    while (program[ip] != '[')
-                        ip -= 1;
+                    var skips: usize = 1;
+                    while (skips != 0) {
+                        ip = math.sub(usize, ip, 1) catch return error.OutOfBounds;
+                        switch (try load(program, ip)) {
+                            '[' => skips -= 1,
+                            ']' => skips += 1,
+                            else => {}
+                        }
+                    }
                 }
             },
-            else => unreachable,
+            else => {},
         }
     }
 }
