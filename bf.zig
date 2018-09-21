41101029314a51064309bca194c5f6844f346be2
Jimmi Holst Christensen <jhc@liab.dk>
2018-09-21 10:19:34 +0200

    Added a function that compiles bf instead of interpreting it

--- bf.zig
+++ bf.zig
@@ -5,68 +5,15 @@ const mem   = std.mem;
 const math  = std.math;
 const debug = std.debug;
 
-pub const MemInStream = struct {
-    memory: []u8,
-    stream: Stream,
-
-    pub const Error = error{};
-    pub const Stream = io.InStream(Error);
-
-    pub fn init(memory: []u8) MemInStream {
-        return MemInStream {
-            .memory = memory,
-            .stream = Stream {
-                .readFn = readFn
-            }
-        };
-    }
-
-    fn readFn(in_stream: *Stream, buffer: []u8) Error!usize {
-        const self = @fieldParentPtr(MemInStream, "stream", in_stream);
-        const bytes_read = math.min(buffer.len, self.memory.len);
-
-        mem.copy(u8, buffer, self.memory[0..bytes_read]);
-        self.memory = self.memory[bytes_read..];
-
-        return bytes_read;
-    }
-};
-
-pub const MemOutStream = struct {
-    memory: []u8,
-    stream: Stream,
-
-    pub const Error = error{};
-    pub const Stream = io.OutStream(Error);
-
-    pub fn init(memory: []u8) MemOutStream {
-        return MemOutStream {
-            .memory = memory,
-            .stream = Stream {
-                .writeFn = writeFn
-            }
-        };
-    }
-
-    fn writeFn(out_stream: *Stream, buffer: []const u8) Error!void {
-        const self = @fieldParentPtr(MemOutStream, "stream", out_stream);
-        const bytes_written = math.min(buffer.len, self.memory.len);
-
-        mem.copy(u8, self.memory[0..bytes_written], buffer);
-        self.memory = self.memory[bytes_written..];
-    }
-};
-
-test "bf.interpret: Hello World" {
+fn testBfMethod(comptime method: var, comptime program: []const u8, result: []const u8) !void {
     var in = []u8{0} ** 1024;
     var out = []u8{0} ** 1024;
     var tape = []u8{0} ** 1024;
 
-    var in_fs = MemInStream.init(in[0..]);
-    var out_fs = MemOutStream.init(out[0..]);
-    const program = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
-    try interpret(program, tape[0..], &in_fs.stream, &out_fs.stream);
-    debug.assert(mem.startsWith(u8, out, "Hello World!\n"));
+    var in_fs = io.SliceInStream.init(in[0..]);
+    var out_fs = io.SliceOutStream.init(out[0..]);
+    try method(program, tape[0..], &in_fs.stream, &out_fs.stream);
+    debug.assert(mem.startsWith(u8, out, result));
 }
 
 fn load(m: []const u8, ptr: usize) !u8 {
@@ -106,7 +53,7 @@ pub fn interpret(program: []const u8, tape: []u8, in_stream: var, out_stream: va
             '.' => try out_stream.writeByte(try load(tape, mp)),
             ',' => try store(tape, mp, try in_stream.readByte()),
             '[' => {
-                if (tape[mp] == 0) {
+                if ((try load(tape, mp)) == 0) {
                     var skips: usize = 1;
                     while (skips != 0) {
                         ip = math.add(usize, ip, 1) catch return error.OutOfBounds;
@@ -119,7 +66,7 @@ pub fn interpret(program: []const u8, tape: []u8, in_stream: var, out_stream: va
                 }
             },
             ']' => {
-                if (tape[mp] != 0) {
+                if ((try load(tape, mp)) != 0) {
                     var skips: usize = 1;
                     while (skips != 0) {
                         ip = math.sub(usize, ip, 1) catch return error.OutOfBounds;
@@ -134,4 +81,61 @@ pub fn interpret(program: []const u8, tape: []u8, in_stream: var, out_stream: va
             else => {},
         }
     }
+}
+
+test "bf.interpret" {
+    try testBfMethod(
+        interpret,
+        "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.",
+        "Hello World!\n",
+    );
+}
+
+pub fn compile(comptime program: []const u8, tape: []u8, in_stream: var, out_stream: var) !void {
+    var mp : usize = 0;
+    return try compileHelper(program, &mp, tape, in_stream, out_stream);
+}
+
+fn compileHelper(comptime program: []const u8, mp: *usize, tape: []u8, in_stream: var, out_stream: var) !void {
+    comptime var ip = 0;
+    inline while (ip < program.len) : (ip += 1) {
+        switch (program[ip]) {
+            '>' => mp.* = math.add(usize, mp.*, 1) catch return error.OutOfBounds,
+            '<' => mp.* = math.sub(usize, mp.*, 1) catch return error.OutOfBounds,
+            '+' => try add(tape, mp.*, 1),
+            '-' => try sub(tape, mp.*, 1),
+            '.' => try out_stream.writeByte(try load(tape, mp.*)),
+            ',' => try store(tape, mp.*, try in_stream.readByte()),
+            '[' => {
+                const start = ip + 1;
+                const end = comptime blk: {
+                    var skips: usize = 1;
+                    while (skips != 0) {
+                        ip += 1;
+                        switch (program[ip]) {
+                            '[' => skips += 1,
+                            ']' => skips -= 1,
+                            else => {}
+                        }
+                    }
+
+                    break :blk ip;
+                };
+
+                while ((try load(tape, mp.*)) != 0) {
+                    try compileHelper(program[start..end], mp, tape, in_stream, out_stream);
+                }
+            },
+            ']' => comptime unreachable,
+            else => {},
+        }
+    }
+}
+
+test "bf.compile" {
+    try testBfMethod(
+        compile,
+        "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.",
+        "Hello World!\n",
+    );
 }
